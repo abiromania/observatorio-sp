1581460b947f644ef77425c81fc463ab15b8d2cf
Abiromania <ze0nxhad0w@protonmail.com>
2025-09-06 19:23:57 -0300

    Inserção do arquivo csv ao banco de dados PostgreSQL

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE observatorio;
+
+CREATE TABLE ocorrencias (
+    id SERIAL PRIMARY KEY,
+    data_ocorrencia DATE,
+    hora TIME,
+    natureza VARCHAR(255),
+    logradouro VARCHAR(255),
+    bairro VARCHAR(255),
+    latittude VARCHAR(50),
+    longitude VARCHAR(50)
+);
+
+CREATE USER abiromania WITH PASSWORD 'abiromnia';
+GRANT ALL PRIVILEGES ON DATABASE observatorio TO abiromania;
