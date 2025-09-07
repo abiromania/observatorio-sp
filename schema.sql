d660b3467b46c420ba48504fbcf5545be65757e1
Abiromania <ze0nxhad0w@protonmail.com>
2025-09-07 17:01:11 -0300

    Alteração criação TABLE

--- schema.sql
+++ schema.sql
@@ -5,11 +5,7 @@ CREATE TABLE ocorrencias (
     data_ocorrencia DATE,
     hora TIME,
     natureza VARCHAR(255),
-    logradouro VARCHAR(255),
     bairro VARCHAR(255),
     latitude VARCHAR(50),
     longitude VARCHAR(50)
 );
-
-CREATE USER abiromania WITH PASSWORD 'abiromnia';
-GRANT ALL PRIVILEGES ON DATABASE observatorio TO abiromania;
